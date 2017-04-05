229e484a9ed4a2ae3d6cde2f842a33a54b24f4ff
David Young <david@thedavidyoung.co.uk>
2017-04-05 12:06:06 +0100

    Udev monitor for usb events

--- detect-usb.py
+++ detect-usb.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+"""
+This part listens for udev events to detect insertion of le usb
+"""
+
+import pyudev
+
+class USBMonitor:
+	def __init__(self):
+		self.context = pyudev.Context()
+		self.monitor = pyudev.Monitor.from_netlink(self.context)
+		self.monitor.filter_by(subsystem='usb')
+		self.monitor.start()
+
 if __name__ == "__main__":
 	print("Entering debug mode...")
 	print("You just lost the game")
+	usbMon = USBMonitor()
+	for device in iter(usbMon.monitor.poll, None):
+		print("something happened...")	
