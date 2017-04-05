0ae5a53bf59224f8316307b23dc2e5f0fab0e29c
David Young <david@thedavidyoung.co.uk>
2017-04-05 14:08:05 +0100

    broken version of udev thingy

--- detect-usb.py
+++ detect-usb.py
@@ -1,10 +1,11 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
 
 """
 This part listens for udev events to detect insertion of le usb
 """
 
 import pyudev
+import os
 
 class USBMonitor:
 	def __init__(self):
@@ -18,4 +19,12 @@ if __name__ == "__main__":
 	print("You just lost the game")
 	usbMon = USBMonitor()
 	for device in iter(usbMon.monitor.poll, None):
-		print("something happened...")	
+		print("something happened...")
+		print(device)
+		for prop in device.__iter__():
+			print(prop,device.__getitem__(prop))
+		with os.scandir("/dev/disk/by-id/") as ls:
+			for entry in ls:
+				if device.__getitem__("ID_SERIAL") in entry.name:
+					print(entry.name)
+			ls.close()
