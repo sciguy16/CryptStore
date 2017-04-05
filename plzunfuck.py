499b642e67c596a65f503bfe5e86c8327a4cd2d2
Unknown <tbc1g14@soton.ac.uk>
2017-04-05 23:40:06 +0100

    Fixing somre more things

--- plzunfuck.py
+++ plzunfuck.py
@@ -24,13 +24,19 @@ class MyWindow(Gtk.Window):
 
         self.decryptButton = Gtk.Button(label="Decrypt")
         self.decryptButton.connect("clicked", self.on_button1_clicked)
-        self.box.pack_start(self.t, True, True, 0)
+        self.box.pack_start(self.decryptButton, True, True, 0)
+
+        self.keySelector = Gtk.Button(label="Decrypt")
+        self.keySelector.connect("clicked", self.on_keyselect_clicked)
+        self.box.pack_start(self.keySelector, True, True, 0)
 
         link = Gtk.LinkButton("https://www.sucss.org/sucss2bu", "OnlineHelp")
         self.box.pack_start(link, True, True, 0)
 
 
-
+    def on_keyselect_clicked(self,widget):
+        print("Opening Key Selector")
+        KeySelectWindow.show_all()
 
     def on_keygen_clicked(self,widget):
         print("Opening Key Generator")
@@ -48,7 +54,7 @@ class MyWindow(Gtk.Window):
         DecryptWindow.show_all()
 
 
-class EntryWindow(Gtk.Window):
+class encryptor(Gtk.Window):
     def __init__(self):
         self.set_border_width(10)
         Gtk.Window.__init__(self, title="Entry Demo")
@@ -123,12 +129,7 @@ class EntryWindow(Gtk.Window):
         print("Bye Bye")
         self.hide()
         absolutely_path = None
-
-
-
-
-
-class keygen(Gtk.Window):
+class keygenor(Gtk.Window):
     def __init__(self):
         self.set_border_width(10)
         Gtk.Window.__init__(self, title="Entry Demo")
@@ -223,16 +224,7 @@ class keygen(Gtk.Window):
         print("Bye Bye")
         self.hide()
         absolutely_path = None
-
-
-
-
-
-
-
-
-
-class Decrypt(Gtk.Window):
+class decryptor(Gtk.Window):
     def __init__(self):
         self.set_border_width(10)
         Gtk.Window.__init__(self, title="Entry Demo")
@@ -305,6 +297,80 @@ class Decrypt(Gtk.Window):
         filter_any.set_name("Any files")
         filter_any.add_pattern("*")
         dialog.add_filter(filter_any)
+class keyselector(Gtk.Window):
+    def __init__(self):
+        self.set_border_width(10)
+        Gtk.Window.__init__(self, title="Select a USB device, then hit continue, and select a key ")
+        self.set_size_request(200, 100)
+
+        self.timeout_id = None
+
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
+        self.add(vbox)
+
+
+        self.entryPassword = Gtk.Entry()
+        self.entryPassword.set_text("Password")
+        vbox.pack_start(self.entryPassword, True, True, 0)
+
+        self.entryFile = Gtk.Button(label="Choose a File")
+        self.entryFile.connect("clicked", self.on_file_clicked)
+        vbox.pack_start(self.entryFile, True, True, 0)
+
+        self.entryEncrypt = Gtk.Button(label="Decrypt")
+        self.entryEncrypt.connect("clicked", self.on_decrypt_clicked)
+        vbox.pack_start(self.entryEncrypt, True, True, 0)
+
+
+        self.entryCancel = Gtk.Button(label="Close")
+        self.entryCancel.connect("clicked", self.on_cancel_clicked)
+        vbox.pack_start(self.entryCancel, True, True, 0)
+
+    def on_cancel_clicked(self, button):
+        print("Bye Bye")
+        self.hide()
+        absolutely_path = None
+    def on_decrypt_clicked(self,button):
+        print("We're now decrypting")
+        print("Password = ", self.entryPassword.get_text())
+
+    def on_file_clicked(self, widget):
+        global absolutely_path
+        dialog = Gtk.FileChooserDialog("Please choose a file or folder", self,
+                                       Gtk.FileChooserAction.OPEN,
+                                       (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
+                                        Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        self.add_filters(dialog)
+
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            print("Open clicked")
+            print("File/Folder selected: " + dialog.get_filename())
+            absolutely_path = dialog.get_filename()
+
+            print(absolutely_path)
+        elif response == Gtk.ResponseType.CANCEL:
+            print("Cancel clicked")
+
+        dialog.destroy()
+
+    def add_filters(self, dialog):
+        filter_text = Gtk.FileFilter()
+        filter_text.set_name("Text files")
+        filter_text.add_mime_type("text/plain")
+        dialog.add_filter(filter_text)
+
+        filter_py = Gtk.FileFilter()
+        filter_py.set_name("Python files")
+        filter_py.add_mime_type("text/x-python")
+        dialog.add_filter(filter_py)
+
+        filter_any = Gtk.FileFilter()
+        filter_any.set_name("Any files")
+        filter_any.add_pattern("*")
+        dialog.add_filter(filter_any)
+
 class PasswordToShort(Gtk.Dialog):
 
     def __init__(self, parent):
@@ -322,17 +388,21 @@ class PasswordToShort(Gtk.Dialog):
 
 
 def usbPlugged(path):
-        print(path)
+    print(path)
+
 
 monThread = detectusb.MonitorThread(usbPlugged)
 absolutely_path = None
 print(sys.version)
 EncryptWindow = encryptor()
 EncryptWindow.connect("delete-event", Gtk.main_quit)
-KeyGeneratorWindow=keygeno()
+KeyGeneratorWindow=keygenor()
 KeyGeneratorWindow.connect("delete-event",Gtk.main_quit)
 DecryptWindow = decryptor()
 DecryptWindow.connect("delete-event", Gtk.main_quit)
+KeySelectWindow = keyselector()
+KeySelectWindow.connect("delete-event", Gtk.main_quit)
+
 #files = FileChooserWindow()
 #files.connect("delete-event", Gtk.main_quit)
 win = MyWindow()
